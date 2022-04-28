46d4e45b03159d16762357ed221a440c014233d6
Franciely Dutra Bastos <CIANDT\francielydb@lnb035689bhz.ciandt.global>
2022-04-28 16:18:52 -0300

    Java 1 Task 6

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,12 +1,8 @@
-import java.text.NumberFormat;
-import java.util.Locale;
-
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        Locale localBrasil = new Locale("pt", "BR");
-        System.out.println("Valor: " + NumberFormat.getCurrencyInstance(localBrasil).format(valor));
+        System.out.printf("Valor: R$%,.2f\n", valor);
         System.out.printf("Taxa: %.2f", taxa);
         System.out.print("%");
     }
