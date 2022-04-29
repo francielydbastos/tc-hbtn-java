9ed50387f3aab0791cfcc4d8a5b667f5cad27ecc
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-04-29 15:09:38 -0300

    Java 2 Task 2

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,9 @@
+public class Caractere {
+    public static boolean ehMaiusculo(char c) {
+        if (c > 64 && c < 91) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
