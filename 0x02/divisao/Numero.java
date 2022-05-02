132c4621fcaabe4137666d46b0d7743721c9366b
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-02 11:21:55 -0300

    Java 3 Task 0

---
+++ 0x02/divisao/Numero.java
@@ -0,0 +1,12 @@
+public class Numero {
+    public static void dividir(int a, int b){
+        int c = 0;
+        try{
+            c = a/b;
+        }catch (ArithmeticException e){
+            System.out.println("Nao eh possivel dividir por zero");
+        }finally {
+            System.out.println(a + " / " + b + " = " + c);
+        }
+    }
+}
