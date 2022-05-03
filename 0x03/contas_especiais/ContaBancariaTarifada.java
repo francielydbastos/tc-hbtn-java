1c476f04ed33c9019c00b3ce9b5ccb4fcd828714
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-03 12:23:29 -0300

    Java 4 Task 2

---
+++ 0x03/contas_especiais/ContaBancariaTarifada.java
@@ -0,0 +1,27 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaTarifada extends ContaBancariaBasica{
+    private int quantidadeTransacoes;
+
+    public ContaBancariaTarifada(String numeracao, double taxaJurosAnual) {
+        super(numeracao, taxaJurosAnual);
+    }
+
+    @Override
+    public void depositar(double valor) throws OperacaoInvalidaException {
+        super.depositar(valor);
+        setSaldo(getSaldo() - 0.1);
+        this.quantidadeTransacoes = getQuantidadeTransacoes() + 1;
+    }
+
+    @Override
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        super.sacar(valor);
+        setSaldo(getSaldo() - 0.1);
+        this.quantidadeTransacoes = getQuantidadeTransacoes() + 1;
+    }
+
+    public int getQuantidadeTransacoes() {
+        return quantidadeTransacoes;
+    }
+}
