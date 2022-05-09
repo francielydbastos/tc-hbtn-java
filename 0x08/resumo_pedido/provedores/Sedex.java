c03a221e544523f13965ceec52de9597d6f9e706
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-09 10:22:09 -0300

    Java 9 Task 1

---
+++ 0x08/resumo_pedido/provedores/Sedex.java
@@ -0,0 +1,17 @@
+package provedores;
+
+public class Sedex implements ProvedorFrete{
+
+    @Override
+    public Frete calcularFrete(double peso, double valor) {
+        double valorFrete = 0;
+
+        if (peso > 1000) {
+            valorFrete = 0.1 * valor;
+        } else {
+            valorFrete = 0.05 * valor;
+        }
+
+        return new Frete(valorFrete, TipoProvedorFrete.SEDEX);
+    }
+}
