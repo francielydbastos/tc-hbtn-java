62c868ba7afbea51f67c31e81e7cb367000b097e
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-10 15:37:19 -0300

    Java 10  Task 0

--- 0x09/agregacao/Empregado.java
+++ 0x09/agregacao/Empregado.java
@@ -38,6 +38,6 @@ public class Empregado {
         System.out.println("Nome: " + getNome());
         System.out.println("Bairro: " + getEndereco().getBairro());
         System.out.println("Cidade: " + getEndereco().getCidade());
-        System.out.println("Pais: " + getEndereco().getPais());
+        System.out.print("Pais: " + getEndereco().getPais());
     }
 }
