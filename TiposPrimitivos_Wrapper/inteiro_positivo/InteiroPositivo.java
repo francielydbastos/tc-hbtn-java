6169d50d528301aaa0e04b52e5b8c29e5881b2ba
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-12 20:36:20 -0300

    Java 12 Task 0

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,39 @@
+package TiposPrimitivos_Wrapper.inteiro_positivo;
+
+public class InteiroPositivo {
+    private int valor;
+
+    public InteiroPositivo(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if (valor < 0) {
+            throw new NumberFormatException("Valor nao eh um valor inteiro positivo");
+        }
+        this.valor = valor;
+    }
+
+    public InteiroPositivo(String valor) {
+        int valorEnvelopado = Integer.parseInt(valor);
+
+        if (valorEnvelopado < 0){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+
+        this.valor = valorEnvelopado;
+    }
+
+    public boolean ehPrimo() {
+        for (int i = 2; i < this.valor; i++){
+            if (this.valor % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
