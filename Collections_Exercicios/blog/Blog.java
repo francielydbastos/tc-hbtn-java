4496a94b87f1fea68348ffa65f82a43b8ce95273
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-18 09:23:33 -0300

    Java 15 Task 0

---
+++ Collections_Exercicios/blog/Blog.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Blog {
+    private ArrayList<Post> posts;
+
+    public Blog() {
+        this.posts = new ArrayList<Post>();
+    }
+
+    public void adicionarPostagem(Post post) {
+        this.posts.add(post);
+    }
+
+    public Set<String> obterTodosAutores() {
+        Set<String> returnAutores = new TreeSet<String>();
+
+        for (Post post : posts) {
+            returnAutores.add(post.getAutor());
+        }
+
+        return returnAutores;
+    }
+
+    public Map<String,Integer> obterContagemPorCategoria() {
+        Map<String,Integer> returnContagem = new TreeMap<String,Integer>();
+
+        for (Post post : posts) {
+            if (returnContagem.containsKey(post.getCategoria())) {
+                Integer previousCount = returnContagem.get(post.getCategoria());
+                returnContagem.put(post.getCategoria(), previousCount + 1);
+            } else {
+                returnContagem.put(post.getCategoria(), 1);
+            }
+        }
+
+        return returnContagem;
+    }
+}
