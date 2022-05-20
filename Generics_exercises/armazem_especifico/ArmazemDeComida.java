304b7a5d3b0f70d4485ed15db25775617d9f9841
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-20 16:25:16 -0300

    Java 17 Task 1

---
+++ Generics_exercises/armazem_especifico/ArmazemDeComida.java
@@ -0,0 +1,7 @@
+public class ArmazemDeComida extends Armazem<Comida>{
+
+    @Override
+    public void adicionarAoInventario(String nome, Comida valor) {
+        super.adicionarAoInventario(nome, valor);
+    }
+}
