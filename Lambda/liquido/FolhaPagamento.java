9e4ddd3a0f128b2f3cad7aa7b2a91c5b6e0c1e46
Franciely Dutra Bastos <francielydbastos@gmail.com>
2022-05-24 14:04:59 -0300

    Java 18 Task 1

---
+++ Lambda/liquido/FolhaPagamento.java
@@ -0,0 +1,6 @@
+public class FolhaPagamento {
+
+    public static double calcularLiquido(Funcionario funcionario, CalculadorDeSalario calculo) {
+        return calculo.calcular(funcionario.getSalario());
+    }
+}
